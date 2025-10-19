e49fdf86543fbcab94ecbbac8844997d4634da93
Nikolay Stanchev <nikist97.ns@icloud.com>
2025-10-19 17:36:43 +0300

    Initial project setup

---
+++ api/src/test/java/io/gendora/AppTest.java
@@ -0,0 +1,12 @@
+package io.gendora;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
